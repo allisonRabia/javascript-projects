f9a2e303d8a4ee6f445bd44f765390dc847f4725
allisonRabia <09snippet-frazzle@icloud.com>
2024-05-01 14:41:01 -0500

    ch6 errors and debugging exercises

--- errors-and-debugging/exercises/DebuggingLogicErrors1.js
+++ errors-and-debugging/exercises/DebuggingLogicErrors1.js
@@ -1,6 +1,21 @@
 // Run this sample code as-is and examine the output. 
-// Should the shuttle have launched? 
-// Did it?
+// exercise instructions
+/* Imagine we are running a space station. Your job is to evaluate the station’s code and fix any errors. The lives of the crew rest squarely upon your shoulders.
+
+Your directions from superiors:
+
+Launch the shuttle only if the fuel, crew and computer all check out OK.
+If a check fails, print that information to the console and scrub the launch.
+If all checks are successful, print a countdown to launch in the console.*/
+
+
+/*$ node debugginglogicerrors1
+WARNING: Insufficient fuel!
+Crew & computer cleared.
+10, 9, 8, 7, 6, 5, 4, 3, 2, 1...
+Liftoff! */
+// Should the shuttle have launched? //no
+// Did it? //yes
 // Do not worry about fixing the code yet, we will do that in the next series of exercises.
 
 let launchReady = false;
