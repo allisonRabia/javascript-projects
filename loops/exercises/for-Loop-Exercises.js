214b5b06d5ba2d1cfbb774ffa3c1eb7bf9c31055
allisonRabia <09snippet-frazzle@icloud.com>
2024-05-13 21:06:56 -0500

    chp9LoopsExercises

--- loops/exercises/for-Loop-Exercises.js
+++ loops/exercises/for-Loop-Exercises.js
@@ -4,21 +4,73 @@
     c. Print the EVEN numbers 12 to -14 in descending order, one number per line.
     d. Challenge - Print the numbers 50 - 20 in descending order, but only if the numbers are multiples of 3. (Your code should work even if you replace 50 or 20 with other numbers). */
 
+    //a
+//     for (let i = 0; i < 21; i++) {
+//       console.log(i);
+//     }
 
+// //b
+// for (let i = 3; i < 30; i = i + 2) {
+//   console.log(i);
+// }
+  
+// //c
+// for (let i = 12; i > -15; i = i - 2) {
+//   console.log(i);
+// }
 
-
+// //d --come back to this
+// for (let i = 50; i > 19 ; i = i + 2) {
+//   console.log(i);
+//}
 /*Exercise #2: 
 Initialize two variables to hold the string “LaunchCode” and the array [1, 5, ‘LC101’, ‘blue’, 42].
-
 
 Construct ``for`` loops to accomplish the following tasks:
   a. Print each element of the array to a new line.
   b. Print each character of the string - in reverse order - to a new line. */ 
+ //initialize and a
+  let str = "LaunchCode";
+  let arr = [1, 5, 'LC101', 'blue', 42];
+  let reversed = "";
 
-
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+ }
+//b
+for (let i = 0; i < str.length; i++) {
+  reversed = str[i] + reversed; //returns eulb
+  //reversed = reversed + str[i];  //returns blue
+}
+console.log(reversed); //prints Launchcode backwards
 
 
 
 /*Exercise #3:Construct a for loop that sorts the array [2, 3, 13, 18, -5, 38, -10, 11, 0, 104] into two new arrays:
   a. One array contains the even numbers, and the other holds the odds.
   b. Print the arrays to confirm the results. */
+let ogArray = [2, 3, 13, 18, -5, 38, -10, 11, 0, 104];
+let evens = [];
+let odds = [];
+console.log(typeof evens);
+
+for (i = 0; i < ogArray.length; i++) {
+  if ((ogArray[i] % 2) === 0) {
+    evens += ogArray[i] + " ";
+  } else {
+    odds += ogArray[i] + " ";
+  }
+}
+console.log(evens);
+console.log(odds);
+console.log(typeof evens);
+
+
+
+
+
+
+
+  // for (let i = 0; i < 100; i++){
+  //  console.log("Repetition is a good thing.");
+  // }
