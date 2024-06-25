fef3fe6963b7feb72981ca9a97016cb08d7a3f43
allisonRabia <09snippet-frazzle@icloud.com>
2024-06-25 15:15:39 -0500

    ch17 exercises

--- exceptions/exercises/divide.js
+++ exceptions/exercises/divide.js
@@ -5,3 +5,16 @@
 // However, if the denominator is zero you should throw the error, "Attempted to divide by zero."
 
 // Code your divide function here:
+const input = require("readline-sync");
+
+let numerator = Number(input.question("Enter a number for a numerator "));
+let denominator = Number(input.question("Enter a number for a denominator "));
+
+console.log(divide(numerator, denominator));
+
+function divide(numerator, denominator) {
+  if (denominator === 0) {
+    throw Error("You cannot divide by zero");
+  }
+  return numerator / denominator;
+}
--- exceptions/exercises/test-student-labs.js
+++ exceptions/exercises/test-student-labs.js
@@ -1,24 +1,55 @@
 function gradeLabs(labs) {
-  for (let i=0; i < labs.length; i++) {
+  for (let i = 0; i < labs.length; i++) {
     let lab = labs[i];
-    let result = lab.runLab(3);
-    console.log(`${lab.student} code worked: ${result === 27}`);
+    try {
+      let result = lab.runLab(3);
+      console.log(`${lab.student} code worked: ${result === 27}`);
+    } catch (err) {
+      result = "Error thrown";
+    }
   }
 }
 
+// Add a try/catch block inside of gradeLabs to catch an exception
+//  if the runLab property is not defined. If the exception is thrown,
+//  result should be set to the text "Error thrown".
+
 let studentLabs = [
   {
-    student: 'Carly',
+    student: "Carly",
     runLab: function (num) {
-        return Math.pow(num, num);
-    }
+      return Math.pow(num, num);
+    },
   },
   {
-    student: 'Erica',
+    student: "Erica",
     runLab: function (num) {
-        return num * num;
-    }
-  }
+      return num * num;
+    },
+  },
 ];
 
 gradeLabs(studentLabs);
+
+let studentLabs2 = [
+  {
+    student: "Blake",
+    myCode: function (num) {
+      return Math.pow(num, num);
+    },
+  },
+  {
+    student: "Jessica",
+    runLab: function (num) {
+      return Math.pow(num, num);
+    },
+  },
+  {
+    student: "Mya",
+    runLab: function (num) {
+      return num * num;
+    },
+  },
+];
+
+gradeLabs(studentLabs2);
